d1e471934967cad26c4b4d85217758bfc4be9bb6
AlexBeovides <abasbeovides@gmail.com>
2024-06-07 02:25:16 -0400

    added automata tools, added grammar productions and non terminals

--- src/common/automata.py
+++ src/common/automata.py
@@ -1,191 +1,41 @@
-try:
-    import pydot
-except:
-    pass
+import pydot
+from common.utils import ContainerSet, DisjointSet
 
-class State:
-    def __init__(self, state, final=False, formatter=lambda x: str(x), shape='circle'):
-        self.state = state
-        self.final = final
-        self.transitions = {}
-        self.epsilon_transitions = set()
-        self.tag = None
-        self.formatter = formatter
-        self.shape = shape
-
-    # The method name is set this way from compatibility issues.
-    def set_formatter(self, value, attr='formatter', visited=None):
-        if visited is None:
-            visited = set()
-        elif self in visited:
-            return
-
-        visited.add(self)
-        self.__setattr__(attr, value)
-        for destinations in self.transitions.values():
-            for node in destinations:
-                node.set_formatter(value, attr, visited)
-        for node in self.epsilon_transitions:
-            node.set_formatter(value, attr, visited)
-        return self
-
-    def has_transition(self, symbol):
-        return symbol in self.transitions
-
-    def add_transition(self, symbol, state):
+class NFA:
+    def __init__(self, states, finals, transitions, start=0):
+        self.states = states
+        self.start = start
+        self.finals = set(finals)
+        self.map = transitions
+        self.vocabulary = set()
+        self.transitions = { state: {} for state in range(states) }
+        
+        for (origin, symbol), destinations in transitions.items():
+            assert hasattr(destinations, '__iter__'), 'Invalid collection of states'
+            self.transitions[origin][symbol] = destinations
+            self.vocabulary.add(symbol)
+            
+        self.vocabulary.discard('')
+        
+    def epsilon_transitions(self, state):
+        assert state in self.transitions, 'Invalid state'
         try:
-            self.transitions[symbol].append(state)
-        except:
-            self.transitions[symbol] = [state]
-        return self
-
-    def add_epsilon_transition(self, state):
-        self.epsilon_transitions.add(state)
-        return self
-
-    def recognize(self, string):
-        states = self.epsilon_closure
-        for symbol in string:
-            states = self.move_by_state(symbol, *states)
-            states = self.epsilon_closure_by_state(*states)
-        return any(s.final for s in states)
-
-    def to_deterministic(self, formatter=lambda x: str(x)):
-        closure = self.epsilon_closure
-        start = State(tuple(closure), any(s.final for s in closure), formatter)
-
-        closures = [ closure ]
-        states = [ start ]
-        pending = [ start ]
-
-        while pending:
-            state = pending.pop()
-            symbols = { symbol for s in state.state for symbol in s.transitions }
-
-            for symbol in symbols:
-                move = self.move_by_state(symbol, *state.state)
-                closure = self.epsilon_closure_by_state(*move)
-
-                if closure not in closures:
-                    new_state = State(tuple(closure), any(s.final for s in closure), formatter)
-                    closures.append(closure)
-                    states.append(new_state)
-                    pending.append(new_state)
-                else:
-                    index = closures.index(closure)
-                    new_state = states[index]
-
-                state.add_transition(symbol, new_state)
-
-        return start
-
-    @staticmethod
-    def from_nfa(nfa, get_states=False):
-        states = []
-        for n in range(nfa.states):
-            state = State(n, n in nfa.finals)
-            states.append(state)
-
-        for (origin, symbol), destinations in nfa.map.items():
-            origin = states[origin]
-            origin[symbol] = [ states[d] for d in destinations ]
-
-        if get_states:
-            return states[nfa.start], states
-        return states[nfa.start]
-
-    @staticmethod
-    def move_by_state(symbol, *states):
-        return { s for state in states if state.has_transition(symbol) for s in state[symbol]}
-
-    @staticmethod
-    def epsilon_closure_by_state(*states):
-        closure = { state for state in states }
-
-        l = 0
-        while l != len(closure):
-            l = len(closure)
-            tmp = [s for s in closure]
-            for s in tmp:
-                for epsilon_state in s.epsilon_transitions:
-                        closure.add(epsilon_state)
-        return closure
-
-    @property
-    def epsilon_closure(self):
-        return self.epsilon_closure_by_state(self)
-
-    @property
-    def name(self):
-        return self.formatter(self.state)
-
-    def get(self, symbol):
-        target = self.transitions[symbol]
-        assert len(target) == 1
-        return target[0]
-
-    def __getitem__(self, symbol):
-        if symbol == '':
-            return self.epsilon_transitions
-        try:
-            return self.transitions[symbol]
+            return self.transitions[state]['']
         except KeyError:
-            return None
-
-    def __setitem__(self, symbol, value):
-        if symbol == '':
-            self.epsilon_transitions = value
-        else:
-            self.transitions[symbol] = value
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return str(self.state)
-
-    def __hash__(self):
-        return hash(self.state)
-
-    def __iter__(self):
-        yield from self._visit()
-
-    def _visit(self, visited=None):
-        if visited is None:
-            visited = set()
-        elif self in visited:
-            return
-
-        visited.add(self)
-        yield self
-
-        for destinations in self.transitions.values():
-            for node in destinations:
-                yield from node._visit(visited)
-        for node in self.epsilon_transitions:
-            yield from node._visit(visited)
-
+            return ()
+            
     def graph(self):
         G = pydot.Dot(rankdir='LR', margin=0.1)
         G.add_node(pydot.Node('start', shape='plaintext', label='', width=0, height=0))
 
-        visited = set()
-        def visit(start):
-            ids = id(start)
-            if ids not in visited:
-                visited.add(ids)
-                G.add_node(pydot.Node(ids, label=start.name, shape=self.shape, style='bold' if start.final else ''))
-                for tran, destinations in start.transitions.items():
-                    for end in destinations:
-                        visit(end)
-                        G.add_edge(pydot.Edge(ids, id(end), label=tran, labeldistance=2))
-                for end in start.epsilon_transitions:
-                    visit(end)
-                    G.add_edge(pydot.Edge(ids, id(end), label='ε', labeldistance=2))
+        for (start, tran), destinations in self.map.items():
+            tran = 'ε' if tran == '' else tran
+            G.add_node(pydot.Node(start, shape='circle', style='bold' if start in self.finals else ''))
+            for end in destinations:
+                G.add_node(pydot.Node(end, shape='circle', style='bold' if end in self.finals else ''))
+                G.add_edge(pydot.Edge(start, end, label=tran, labeldistance=2))
 
-        visit(self)
-        G.add_edge(pydot.Edge('start', id(self), label='', style='dashed'))
-
+        G.add_edge(pydot.Edge('start', self.start, label='', style='dashed'))
         return G
 
     def _repr_svg_(self):
@@ -194,14 +44,229 @@ class State:
         except:
             pass
 
-    def write_to(self, fname):
-        return self.graph().write_svg(fname)
+class DFA(NFA):
+    
+    def __init__(self, states, finals, transitions, start=0):
+        assert all(isinstance(value, int) for value in transitions.values())
+        assert all(len(symbol) > 0 for origin, symbol in transitions)
+        
+        transitions = { key: [value] for key, value in transitions.items() }
+        NFA.__init__(self, states, finals, transitions, start)
+        self.current = start
+        
+    def _move(self, symbol):
+        try:
+            self.current = self.transitions[self.current][symbol][0]
+            return True
+        except KeyError:
+            return False
+    
+    def _reset(self):
+        self.current = self.start
+        
+    def recognize(self, string):
+        self._reset()
+        for symbol in string:
+            if not self._move(symbol):
+                return False
+        return self.current in self.finals
+                
+def move(automaton, states, symbol):
+    moves = set()
+    for state in states:
+        assert state in automaton.transitions, 'Invalid state'
+        try:
+            res = automaton.transitions[state][symbol]
+            for dest in res:
+                moves.add(dest)
+        except KeyError:
+            pass
+    return moves
 
-def multiline_formatter(state):
-    return '\n'.join(str(item) for item in state)
+def epsilon_closure(automaton, states):
+    pending = [ s for s in states ] # equivalente a list(states) pero me gusta así :p
+    closure = { s for s in states } # equivalente a  set(states) pero me gusta así :p
+    
+    while pending:
+        state = pending.pop()
+        for dest in automaton.epsilon_transitions(state):
+            closure.add(dest)
+            pending.append(dest)
+                
+    return ContainerSet(*closure)
 
-def lr0_formatter(state):
-    try:
-        return '\n'.join(str(item)[:-4] for item in state)
-    except TypeError:
-        return str(state)[:-4]
+def nfa_to_dfa(automaton):
+    transitions = {}
+    
+    start = epsilon_closure(automaton, [automaton.start])
+    start.id = 0
+    start.is_final = any(s in automaton.finals for s in start)
+    states = [ start ]
+
+    pending = [ start ]
+    while pending:
+        state = pending.pop()
+        for symbol in automaton.vocabulary:
+            dests = move(automaton, state, symbol)
+            dests = epsilon_closure(automaton, dests)
+            id = -1
+            for i in range(len(states)):
+                if dests == states[i]:
+                    id = i
+                    break
+            if len(dests):
+                if id != -1:
+                    new_state = states[id]
+                else:
+                    new_state = ContainerSet(*dests)
+                    new_state.id = len(states)
+                    new_state.is_final = any(s in automaton.finals for s in new_state)
+                    states.append(new_state)
+                    pending.append(new_state)
+                
+                transitions[state.id, symbol] = new_state.id
+            '''
+            try:
+                transitions[state.id, symbol]
+                assert False, 'Invalid DFA!!!'
+            except KeyError:
+                # Your code here
+                pass'''
+    
+    finals = [ state.id for state in states if state.is_final ]
+    dfa = DFA(len(states), finals, transitions)
+    return dfa
+
+def automata_union(a1, a2):
+    transitions = {}
+    
+    start = 0
+    d1 = 1
+    d2 = a1.states + d1
+    final = a2.states + d2
+    
+    for (origin, symbol), destinations in a1.map.items():
+        transitions[(origin + d1, symbol)] = [dest + d1 for dest in destinations]
+
+    for (origin, symbol), destinations in a2.map.items():
+        transitions[(origin + d2, symbol)] = [dest + d2 for dest in destinations]
+    
+    transitions[(start, '')] = [a1.start + d1, a2.start + d2]
+    
+    for f in a1.finals:
+        transitions[(f + d1, '')] = [final]
+
+    for f in a2.finals:
+        transitions[(f + d2, '')] = [final]
+            
+    states = a1.states + a2.states + 2
+    finals = { final }
+    
+    return NFA(states, finals, transitions, start)
+
+def automata_concatenation(a1, a2):
+    transitions = {}
+    
+    start = 0
+    d1 = 0
+    d2 = a1.states + d1
+    final = a2.states + d2
+    
+    for (origin, symbol), destinations in a1.map.items():
+        transitions[(origin + d1, symbol)] = [dest + d1 for dest in destinations]
+
+    for (origin, symbol), destinations in a2.map.items():
+        transitions[(origin + d2, symbol)] = [dest + d2 for dest in destinations]
+    
+    for f in a1.finals:
+        transitions[(f + d1, '')] = [a2.start + d2]
+
+    for f in a2.finals:
+        transitions[(f + d2, '')] = [final]
+            
+    states = a1.states + a2.states + 1
+    finals = { final }
+    
+    return NFA(states, finals, transitions, start)
+
+def automata_closure(a1):
+    transitions = {}
+    
+    start = 0
+    d1 = 1
+    final = a1.states + d1
+    
+    for (origin, symbol), destinations in a1.map.items():
+        transitions[(origin + d1, symbol)] = [dest + d1 for dest in destinations]
+    
+    transitions[(start, '')] = [a1.start + d1, final]
+    
+    for f in a1.finals:
+        try:
+            transitions[(f + d1, '')] += [start]
+        except KeyError:
+            transitions[(f + d1, '')] = [start]
+            
+    states = a1.states +  2
+    finals = { final }
+    
+    return NFA(states, finals, transitions, start)
+
+
+def distinguish_states(group, automaton, partition):
+    split = {}
+    vocabulary = tuple(automaton.vocabulary)
+
+    for member in group:
+        transitions = automaton.transitions[member.value]
+        labels = ((transitions[symbol][0] if symbol in transitions else None) for symbol in vocabulary)
+        key = tuple((partition[node].representative if node in partition.nodes else None) for node in labels)
+        try:
+            split[key].append(member.value)
+        except KeyError:
+            split[key] = [member.value]
+
+    return [ group for group in split.values()]
+            
+def state_minimization(automaton):
+    partition = DisjointSet(*range(automaton.states))
+    
+    ## partition = { NON-FINALS | FINALS }
+    partition.merge(automaton.finals)
+    partition.merge(q for q in range(automaton.states) if q not in automaton.finals)
+    
+    while True:
+        new_partition = DisjointSet(*range(automaton.states))
+        for group in partition.groups:
+            distinguished = distinguish_states(group, automaton, partition)
+            for subgroup in distinguished:
+                new_partition.merge(subgroup)
+
+        if len(new_partition) == len(partition):
+            break
+
+        partition = new_partition
+        
+    return partition
+
+def automata_minimization(automaton):
+    partition = state_minimization(automaton)
+    
+    states = [s for s in partition.representatives]
+    
+    transitions = {}
+    for i, state in enumerate(states):
+        origin = state.value
+        for symbol, destinations in automaton.transitions[origin].items():
+            representative = partition[destinations[0]].representative
+            j = states.index(representative)
+            try:
+                transitions[i, symbol]
+                assert False
+            except KeyError:
+                transitions[i, symbol] = j
+    
+    start = states.index(partition[automaton.start].representative)
+    finals = [i for i, state in enumerate(states) if state.value in automaton.finals]
+
+    return DFA(len(states), finals, transitions, start)
--- src/parser/grammar.py
+++ src/parser/grammar.py
@@ -7,24 +7,53 @@ G = Grammar()
 # non-terminals
 program = G.NonTerminal('<program>', startSymbol=True)
 
-class_list, def_class = G.NonTerminals('<class-list> <def-class>')
-feature_list, def_attr, def_func = G.NonTerminals('<feature-list> <def-attr> <def-func>')
-param_list, param, expr_list = G.NonTerminals('<param-list> <param> <expr-list>')
-expr, arith, term, factor, atom = G.NonTerminals('<expr> <arith> <term> <factor> <atom>')
-func_call, arg_list  = G.NonTerminals('<func-call> <arg-list>')
+definition_list = G.NonTerminal('<definition-list>') 
+definition = G.NonTerminal('<definition>') 
+func_def = G.NonTerminal('<func-def>') 
+type_def = G.NonTerminal('<type-def>') 
+
+expr = G.NonTerminal('<expr>') 
+simple_expr = G.NonTerminal('<simple-expr>') 
+block_expr = G.NonTerminal('<block-expr>') 
+expr_list = G.NonTerminal('<expr-list>') 
+lego_expr = G.NonTerminal('<lego-expr>') 
 
 
 # terminals
+type_, inherits, base, idx, new, is_ , as_ = G.Terminals('type inherits base <id> new is as')
+let, in_, equal, d_assign = G.Terminals('let in = :=')
+function, arrow = G.Terminal('function =>')
+if_, elif_, else_ = G.Terminals('if elif else')
+while_, for_ = G.Terminals('while for')
 plus, minus, star, div, mod, pow, pow2, number_lit = G.Terminals('+ - * / % ^ ** <number>')
 eq, neq, le, ge, lt, gt = G.Terminals('== != <= >= < >')
-semi, colon, comma, dot, opar, cpar, ocur, ccur = G.Terminals('; : , . ( ) { }')
 and_ , or_, not_, bool_lit = G.Terminals('& | ! <bool>')
 at, double_at, string_lit = G.Terminals('@ @@ <string>')
-type_, inherits, base, idx, new, is_ , as_ = G.Terminals('type inherits base <id> new is as')
-if_, elif_, else_ = G.Terminals('if elif else')
-let, in_, equal, assign = G.Terminals('let in = :=')
-while_, for_ = G.Terminals('while for')
-function, arrow = G.Terminal('function =>')
+semi, colon, comma, dot, opar, cpar, ocur, ccur = G.Terminals('; : , . ( ) { }')
 
 # productions
-program %= class_list, lambda h,s: ProgramNode(s[1])
+program %= definition_list + simple_expr, lambda h,s: ast.ProgramNode(s[1], s[2])
+program %= definition_list + lego_expr, lambda h,s: ast.ProgramNode(s[1], s[2])
+program %= simple_expr, lambda h,s: ast.ProgramNode([], s[1])
+program %= lego_expr, lambda h,s: ast.ProgramNode([], s[1])
+
+definition_list %= definition + definition_list, lambda h,s: [s[1]] + s[2]
+definition_list %= definition, lambda h,s: [s[1]] 
+
+definition %= func_def, lambda h,s: s[1]
+definition %= type_def, lambda h,s: s[1]          
+
+expr %= simple_expr, lambda h,s: s[1]
+expr %= block_expr, lambda h,s: s[1]
+
+# simple_expr %= 
+
+block_expr %= ocur + expr_list + ccur, lambda h,s: ast.ExpressionBlockNode(s[2])
+
+expr_list %= lego_expr + expr_list, lambda h,s: [s[1]] + s[2]
+expr_list %= lego_expr, lambda h,s: [s[1]] 
+expr_list %= simple_expr, lambda h,s: [s[1]]
+
+lego_expr %= expr + semi, lambda h,s: s[1]
+lego_expr %= block_expr, lambda h,s: s[1]
+
